2538db8ef3a5bab82447c947e0bac22c4aa5d3c7
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-07-11 21:17:15 +0200

    gallery stylization

--- src/page-2.js
+++ src/page-2.js
@@ -15,7 +15,6 @@ const createUrl = (value) => {
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-    per_page: 100,
     });
     
     return `https://pixabay.com/api/?${params}`;
@@ -24,9 +23,21 @@ const createUrl = (value) => {
 const lightbox = new SimpleLightbox('.gallery a');
 
 const createListItemToAdd = (arr) => {
-    const image = arr.map(image => `<li><a href="${image.largeImageURL}"><img width="200px" src="${image.webformatURL}" alt="${image.tags}"></a></li>`).join('');
+    const image = arr.map(image => `<li>
+        <div class="card">
+        <a href="${image.largeImageURL}"><img class="gallery_img" width ="360px" height="200px" src="${image.webformatURL}" alt="${image.tags}"></a>
+        <div class="information_box">
+        <p class="information"><span class="information_header">Likes:</span>${image.likes}</p>
+        <p class="information"><span class="information_header">Views:</span>${image.views}</p>
+        <p class="information"><span class="information_header">Comments:</span>${image.comments}</p>
+        <p class="information"><span class="information_header">Downloads:</span>${image.downloads}</p></div>
+        </div>
+        </li>`).join('');
     gallery.insertAdjacentHTML("afterbegin", image);
     lightbox.refresh();
+}
+const celarFunc = (form) => {
+    form.reset();
 }
 
 const sendRequest = (event) => {
@@ -42,8 +53,11 @@ const sendRequest = (event) => {
             return res.json();
         })
         .then((images) => {
+            
             const imagesArr = images.hits;
+            console.log(imagesArr)
             if (imagesArr.length === 0) {
+                gallery.textContent = "";
                 iziToast.error({
                 title: '',
                 message: 'Sorry, there are no images matching your search query. Please try again!',
@@ -52,6 +66,7 @@ const sendRequest = (event) => {
             }
             
             createListItemToAdd(imagesArr);
+            celarFunc(formEl);
         })
         .catch(error => console.log(error));
 }
